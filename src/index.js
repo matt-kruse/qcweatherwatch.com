1b0f54777e088a08f7c619d8091f1e4e7a422551
Michael Liendo <model3volution@gmail.com>
2018-05-18 16:10:28 -0500

    chore: cleanup for reusability

--- src/index.js
+++ src/index.js
@@ -1,37 +1,24 @@
 import $ from 'jquery'
 import {kwqc, wqad} from './weather-constants'
 
-function scrapeWeather(endpoint, searchCriteria) {
-  $.get(endpoint + encodeURIComponent(searchCriteria) + '@html')
+function scrapeWeather({scrapeLink, searchCriteria, stopWord, htmlLocationName}) {
+  $.get(scrapeLink + encodeURIComponent(searchCriteria) + '@html')
   .done(function(data = {}) {
-    buildHTML(data.content)
+    buildHTML(data.content, stopWord, htmlLocationName)
   })
 }
 
-scrapeWeather(wqad.scrapeLink, wqad.searchCriteria)
+function buildHTML(content, stopWord, placeInHTML) {
+  let html = ''
+  
+  content.every(function(paragraph) {
+    const atEnd = !paragraph.startsWith(stopWord)
 
+    return atEnd ? html += '<p>' + paragraph + '</p>': atEnd
+  })
 
-
-function buildHTML(content) {
-  var html = '';
-  for (var i = 0; i < content.length; i++) {
-    if (content[i].indexOf('Chief') >= 0) {
-      break;
-    }
-    html += '<p>' + content[i]; + '</p>';
-  }
-  $('#wqad-forecast').html(html);
+  $(placeInHTML).html(html)
 }
 
-$.get( kwqc.scrapeLink + encodeURIComponent("#wxAccordion_forecast .media-body") + '@html', function (data = {}) {
-  if (data.content) {
-    var html = '';
-    for (var i = 0; i < data.content.length; i++) {
-      if (data.content[i].indexOf('smart phone') >= 0) {
-        break;
-      }
-      html += '<p>' + data.content[i]; + '</p>';
-    }
-    $('#kwqc-forecast').html(html);
-  }
-});
+scrapeWeather(kwqc)
+scrapeWeather(wqad)
--- src/weather-constants.js
+++ src/weather-constants.js
@@ -1,8 +1,12 @@
 export const wqad= {
   scrapeLink:'https://8xd8ar771c.execute-api.us-east-1.amazonaws.com/prod/xray?url=http://wqad.com/weather/forecast/&selector=',
-  searchCriteria: '.entry-content>div:not([class]), .entry-content>p+p'
+  searchCriteria: '.entry-content>div:not([class]), .entry-content>p+p',
+  stopWord:'Chief',
+  htmlLocationName:'#wqad-forecast'
 }
 export const kwqc= {
   scrapeLink: 'https://8xd8ar771c.execute-api.us-east-1.amazonaws.com/prod/xray?url=http://www.kwqc.com/weather/&selector=',
-  searchCriteria: '#wxAccordion_forecast .media-body'
+  searchCriteria: '#wxAccordion_forecast .media-body',
+  stopWord: 'smart phone',
+  htmlLocationName:'#kwqc-forecast'
 }
